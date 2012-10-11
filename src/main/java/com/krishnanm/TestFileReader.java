e80e5f941e0a86c275918365c2fd773e7c118d41
Krishnan Mani <km@krishnanm.com>
2012-10-11 14:01:10 -0400

    Using a configuration to get the FileSystem instance

--- src/main/java/com/krishnanm/TestFileReader.java
+++ src/main/java/com/krishnanm/TestFileReader.java
@@ -2,11 +2,32 @@ package com.krishnanm;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import java.io.IOException;
+import java.net.URI;
 
 public class TestFileReader {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("File separator: " + org.apache.hadoop.fs.Path.SEPARATOR);
     System.out.println("Path separator: " + org.apache.hadoop.fs.Path.SEPARATOR_CHAR);
+
+    String basePath = "/user/cloudera";
+    String trailingPath = "testdir";
+    Path fullPath = new Path(basePath, trailingPath);
+    System.out.println(fullPath.toString());
+
+    Configuration conf = new Configuration();
+    System.out.println(conf.toString());
+
+    FileSystem localFileSystem = FileSystem.getLocal(conf);
+    System.out.println(localFileSystem.toString());
+/*
+    URI namenode = URI.create("hdfs://0.0.0.0:8020");
+
+    FileSystem hdfs = FileSystem.get(namenode, conf);
+    System.out.println(hdfs.toString());
+*/
   }
 }
